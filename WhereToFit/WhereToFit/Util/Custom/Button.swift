32b0837abf88d5a42f5a8a705e4f8433cc0dfc29
Bambu <byrr113@gmail.com>
2026-05-28 12:26:37 +0900

    ✨feat: Custom Button 속성 정의

--- WhereToFit/WhereToFit/Util/Custom/Button.swift
+++ WhereToFit/WhereToFit/Util/Custom/Button.swift
@@ -9,5 +9,21 @@ import UIKit
 import Then
 
 class Button: UIControl {
+    private let background: UIView
+    private let titleLabel: UILabel
+    private let padding: UIEdgeInsets
     
+    var title: String?  {
+        get { titleLabel.text }
+        set { titleLabel.text = newValue}
+    }
+    
+    init(config: ButtonConfiguration) {
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
