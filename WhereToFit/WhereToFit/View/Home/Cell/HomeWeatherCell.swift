8539b9e0501a82b9b0a002619c391e07a4839503
Bambu <byrr113@gmail.com>
2026-05-30 15:56:23 +0900

    🚚build: HomeWeatherCell 생성

---
+++ WhereToFit/WhereToFit/View/Home/Cell/HomeWeatherCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeWeatherCell.swift
+//  WhereToFit
+//
+//  Created by 변예린 on 5/30/26.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class HomeWeatherCell: UICollectionViewCell {
+    
+}
