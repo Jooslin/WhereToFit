872b4dea6fd744c102e3335046f69820dd4316e9
Bambu <byrr113@gmail.com>
2026-06-02 22:05:13 +0900

    ✨feat: Weather Entity 이니셜라이저 추가, Weathercategory API에 맞추어 보충

--- WhereToFit/WhereToFit/Entity/Weather.swift
+++ WhereToFit/WhereToFit/Entity/Weather.swift
@@ -11,6 +11,52 @@ struct Weather {
     let temperature: Double // 현재 온도
     
     enum WeatherCategory {
-        case sun, rain, cloud, snow, wind
+        case thunderstorm
+        case drizzle
+        case sun
+        case rain
+        case cloud
+        case snow
+        case wind
+        case atmosphere // 대기 관련 - ex. 황사, 안개 등
+        case unknown
+    }
+}
+
+extension Weather {
+    init(dto: CurrentWeatherDTO) {
+        let primaryCondition = dto.weather.first
+        
+        self.category = WeatherCategory(conditionId: primaryCondition?.id)
+        self.description = primaryCondition?.description ?? ""
+        self.temperature = dto.main.temperature
+    }
+}
+
+private extension Weather.WeatherCategory {
+    init(conditionId: Int?) {
+        guard let conditionId else {
+            self = .unknown
+            return
+        }
+        
+        switch conditionId {
+        case 200...299:
+            self = .thunderstorm
+        case 300...399:
+            self = .drizzle
+        case 500...599:
+            self = .rain
+        case 600...699:
+            self = .snow
+        case 700...799:
+            self = .atmosphere
+        case 800:
+            self = .sun
+        case 801...804:
+            self = .cloud
+        default:
+            self = .unknown
+        }
     }
 }
