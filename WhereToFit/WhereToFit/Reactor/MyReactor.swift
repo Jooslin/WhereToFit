976e00f908f4d88ad796ebd3ff7754c8d679c5d6
yeseul jang <yyssjj9999@gmail.com>
2026-06-03 11:29:04 +0900

    🌟feat: 마이페이지 리액터 추가

---
+++ WhereToFit/WhereToFit/Reactor/MyReactor.swift
@@ -0,0 +1,14 @@
+//
+//  MyReactor.swift
+//  WhereToFit
+//
+//  Created by Yeseul Jang on 6/2/26.
+//
+import ReactorKit
+
+final class MyReactor: BaseReactor {
+    let initialState = State()
+
+    enum Action {}
+    struct State {}
+}
