7ad5ab178796e57520b6e907f572fd306fa49c0e
Bambu <byrr113@gmail.com>
2026-06-04 21:39:39 +0900

    ✨feat: OnboardingVC, Onboarding Reactor 생성

---
+++ WhereToFit/WhereToFit/Reactor/OnboardingReactor.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingReactor.swift
+//  WhereToFit
+//
+//  Created by 변예린 on 6/4/26.
+//
+
+import ReactorKit
+
+final class OnboardingReactor: BaseReactor {
+    let initialState: State = State()
+    
+    enum Action {}
+    
+    struct State {}
+}
---
+++ WhereToFit/WhereToFit/View/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardViewController.swift
+//  WhereToFit
+//
+//  Created by 변예린 on 6/4/26.
+//
+
+final class OnboardingViewController: BaseViewController<OnboardingReactor> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func bind(reactor: TempReactor) {
+       
+    }
+    
+}
