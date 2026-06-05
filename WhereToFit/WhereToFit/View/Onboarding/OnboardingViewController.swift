34459f33ba91a0bc6c4fa57fcaeafbcbe7d0491c
Bambu <byrr113@gmail.com>
2026-06-06 01:12:21 +0900

    ✨feat: OnboardingGoalView 생성 및 레이아웃 구성 완료

--- WhereToFit/WhereToFit/View/Onboarding/OnboardingViewController.swift
+++ WhereToFit/WhereToFit/View/Onboarding/OnboardingViewController.swift
@@ -11,7 +11,7 @@ import RxSwift
 import ReactorKit
 
 final class OnboardingViewController: BaseViewController<OnboardingReactor> {
-    let onboardingView = OnboardingExperienceView()
+    let onboardingView = OnboardingGoalView()
     override func loadView() {
         view = onboardingView
     }
