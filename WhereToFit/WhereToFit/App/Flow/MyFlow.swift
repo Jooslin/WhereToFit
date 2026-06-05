60d9b6e9375b34050be5f2cd72a2075142ca06af
yeseul jang <yyssjj9999@gmail.com>
2026-06-05 14:25:16 +0900

    🌟feat: Mytab내에서 뒤로가기 프로필 관리 케이스 구현

--- WhereToFit/WhereToFit/App/Flow/MyFlow.swift
+++ WhereToFit/WhereToFit/App/Flow/MyFlow.swift
@@ -12,19 +12,28 @@ import ReactorKit
 final class MyFlow: Flow {
     private let navigationController = UINavigationController()
     var root: any RxFlow.Presentable { navigationController }
-    
+
     func navigate(to step: any RxFlow.Step) -> RxFlow.FlowContributors {
         // 정의한 AppStep일 때만 동작
         guard let step = step as? AppStep else {
             return .none
         }
-        
+
         switch step {
         case .myTab:
             let vc = MyViewController(reactor: MyReactor())
             navigationController.pushViewController(vc, animated: true)
             return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: vc))
-            
+
+        case .profileManagement:
+            let vc = ProfileManagementViewController(reactor: ProfileManagementReactor())
+            navigationController.pushViewController(vc, animated: true)
+            return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: vc))
+
+        case .pageBack:
+            navigationController.popViewController(animated: true)
+            return .none
+
         default:
             return .one(flowContributor: .forwardToParentFlow(withStep: step))
         }
