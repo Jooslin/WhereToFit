8c958eb8b48b5db54f9f30219d59d87160e49f77
yeseul jang <yyssjj9999@gmail.com>
2026-06-09 13:10:22 +0900

    🚚build: 찜한 프로그램 리액터 추가 및 기본 구현

---
+++ WhereToFit/WhereToFit/Reactor/FavoriteProgramsReactor.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteProgramsReactor.swift
+//  WhereToFit
+//
+//  Created by Yeseul Jang on 6/9/26.
+//
+
+import ReactorKit
+
+final class FavoriteProgramsReactor: BaseReactor {
+    let initialState = State()
+
+    enum Action {}
+    struct State {}
+}
