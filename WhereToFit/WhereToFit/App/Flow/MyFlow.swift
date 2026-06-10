c5ca4691610090087eb86bef307af9489c932d05
yeseul jang <yyssjj9999@gmail.com>
2026-06-10 14:18:32 +0900

    🩹chore:  네이밍 변경

--- WhereToFit/WhereToFit/App/Flow/MyFlow.swift
+++ WhereToFit/WhereToFit/App/Flow/MyFlow.swift
@@ -38,7 +38,7 @@ final class MyFlow: Flow {
             return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: vc))
 
         case .favoritePrograms:
-            let vc = FavoriteListViewController(reactor: FavoriteProgramsReactor())
+            let vc = FavoriteListViewController(reactor: FavoriteListReactor())
             vc.hidesBottomBarWhenPushed = true
             navigationController.pushViewController(vc, animated: true)
             return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: vc))
