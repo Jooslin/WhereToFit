f520c425855b68b114fdc9f0f75cae72e45d4f0b
yeseul jang <yyssjj9999@gmail.com>
2026-06-10 14:52:35 +0900

    🚚build: 운동 결과 리액터 추가 및 기본 구현

---
+++ WhereToFit/WhereToFit/Reactor/ExerciseResultReactor.swift
@@ -0,0 +1,42 @@
+//
+//  ExerciseResultReactor.swift
+//  WhereToFit
+//
+//  Created by Yeseul Jang on 6/10/26.
+//
+
+import ReactorKit
+
+final class ExerciseResultReactor: BaseReactor {
+    let initialState = State(
+        summaryItems: [
+            SummaryItem(title: "운동 목적", value: "건강 관리"),
+            SummaryItem(title: "선호 운동", value: "걷기 / 요가"),
+            SummaryItem(title: "선호 시간", value: "저녁"),
+            SummaryItem(title: "운동 강도", value: "초급")
+        ],
+        recommendationItems: [
+            RecommendationItem(title: "저녁 요가", tags: ["초급", "실내"], imageName: "Gym"),
+            RecommendationItem(title: "체조", tags: ["초급", "야외"], imageName: "Gym"),
+            RecommendationItem(title: "초급 수영", tags: ["초급", "실내"], imageName: "Gym")
+        ]
+    )
+
+    enum Action {}
+
+    struct SummaryItem: Equatable {
+        let title: String
+        let value: String
+    }
+
+    struct RecommendationItem: Equatable {
+        let title: String
+        let tags: [String]
+        let imageName: String
+    }
+
+    struct State {
+        var summaryItems: [SummaryItem]
+        var recommendationItems: [RecommendationItem]
+    }
+}
