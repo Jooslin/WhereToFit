2359ba3009ba50395bb0c5c7b28190931d5e60af
Bambu <byrr113@gmail.com>
2026-06-11 00:22:44 +0900

    ✨feat: Weather Enitty description 수정

--- WhereToFit/WhereToFit/Entity/Weather.swift
+++ WhereToFit/WhereToFit/Entity/Weather.swift
@@ -4,11 +4,22 @@
 //
 //  Created by 변예린 on 6/2/26.
 //
+import UIKit
 
 struct Weather: Hashable {
     let category: WeatherCategory
-    let description: String // 날씨 부가설명
     let temperature: Double // 현재 온도
+    
+    var description: String {
+        switch category {
+        case .thunderstorm, .drizzle, .rain, .snow, .wind, .atmosphere:
+            return "오늘은 실내운동하기 좋은 날"
+        case .sun, .cloud:
+            return "오늘은 야외운동하기 좋은 날"
+        case .unknown:
+            return "오늘의 운동 장소를 확인해보세요"
+        }
+    }
     
     enum WeatherCategory {
         case thunderstorm
@@ -21,14 +32,15 @@ struct Weather: Hashable {
         case atmosphere // 대기 관련 - ex. 황사, 안개 등
         case unknown
     }
+    
 }
 
 extension Weather {
     init(dto: CurrentWeatherDTO) {
         let primaryCondition = dto.weather.first
+        let category = WeatherCategory(conditionId: primaryCondition?.id)
         
-        self.category = WeatherCategory(conditionId: primaryCondition?.id)
-        self.description = primaryCondition?.description ?? ""
+        self.category = category
         self.temperature = dto.main.temperature
     }
 }
