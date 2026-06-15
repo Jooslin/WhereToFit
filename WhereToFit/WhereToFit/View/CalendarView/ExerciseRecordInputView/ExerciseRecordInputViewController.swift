b49adb1643f687a1f0b850ee8c343bb103e4fa06
yeseul jang <yyssjj9999@gmail.com>
2026-06-15 15:34:42 +0900

    🌟feat: 운동 시간 추가 바인딩 구현

--- WhereToFit/WhereToFit/View/CalendarView/ExerciseRecordInputView/ExerciseRecordInputViewController.swift
+++ WhereToFit/WhereToFit/View/CalendarView/ExerciseRecordInputView/ExerciseRecordInputViewController.swift
@@ -5,31 +5,37 @@
 //  Created by Yeseul Jang on 6/15/26.
 //
 
+import ReactorKit
 import RxCocoa
 import RxSwift
 import UIKit
 
-final class ExerciseRecordInputViewController: UIViewController {
+final class ExerciseRecordInputViewController: BaseViewController<ExerciseRecordInputReactor> {
     private let exerciseRecordInputView = ExerciseRecordInputView()
-    private let disposeBag = DisposeBag()
 
     override func loadView() {
         view = exerciseRecordInputView
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        bind()
-    }
-}
-
-private extension ExerciseRecordInputViewController {
-    func bind() {
+    
+    override func bind(reactor: ExerciseRecordInputReactor) {
         exerciseRecordInputView.rx.customButtonTap
-            .bind(with: self) { owner, _ in
-                owner.exerciseRecordInputView.toggleCustomInput()
-            }
+            .map { ExerciseRecordInputReactor.Action.customButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        exerciseRecordInputView.rx.durationFieldTap
+            .map { ExerciseRecordInputReactor.Action.durationFieldTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        exerciseRecordInputView.rx.durationPickerChanged
+            .map { ExerciseRecordInputReactor.Action.durationPickerChanged($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        exerciseRecordInputView.rx.durationPickerSelectButtonTap
+            .map { ExerciseRecordInputReactor.Action.durationPickerSelectButtonTapped }
+            .bind(to: reactor.action)
             .disposed(by: disposeBag)
 
         exerciseRecordInputView.closeButton.rx.tap
@@ -41,6 +47,42 @@ private extension ExerciseRecordInputViewController {
         exerciseRecordInputView.saveButton.rx.tap
             .bind(with: self) { owner, _ in
                 owner.dismiss(animated: true)
+            }
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.isCustomInputVisible)
+            .distinctUntilChanged()
+            .observe(on: MainScheduler.instance)
+            .bind(with: self) { owner, isVisible in
+                owner.exerciseRecordInputView.updateCustomInputVisible(isVisible)
+            }
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.isDurationPickerVisible)
+            .distinctUntilChanged()
+            .observe(on: MainScheduler.instance)
+            .bind(with: self) { owner, isVisible in
+                owner.exerciseRecordInputView.updateDurationPickerVisible(isVisible)
+            }
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.selectedDuration)
+            .distinctUntilChanged()
+            .observe(on: MainScheduler.instance)
+            .bind(with: self) { owner, duration in
+                owner.exerciseRecordInputView.updateSelectedDuration(duration)
+            }
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.confirmedDuration)
+            .distinctUntilChanged()
+            .observe(on: MainScheduler.instance)
+            .bind(with: self) { owner, duration in
+                owner.exerciseRecordInputView.updateConfirmedDuration(duration)
             }
             .disposed(by: disposeBag)
     }
