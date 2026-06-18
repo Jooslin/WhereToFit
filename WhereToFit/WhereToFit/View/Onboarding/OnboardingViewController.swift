99235578a036491e5905ede403f5be26935aabf8
Bambu <byrr113@gmail.com>
2026-06-19 01:48:43 +0900

    ✨feat: OnboardingEndView 생성 및 구현
    - OnboardingStartView 이미지 교체

--- WhereToFit/WhereToFit/View/Onboarding/OnboardingViewController.swift
+++ WhereToFit/WhereToFit/View/Onboarding/OnboardingViewController.swift
@@ -11,7 +11,7 @@ import RxSwift
 import ReactorKit
 
 final class OnboardingViewController: BaseViewController<OnboardingReactor> {
-    let onboardingView = OnboardingFacilityView()
+    let onboardingView = OnboardingEndView()
     override func loadView() {
         view = onboardingView
     }
@@ -48,10 +48,10 @@ final class OnboardingViewController: BaseViewController<OnboardingReactor> {
             homepageURL: "https://www.gumi.go.kr"
         )
         
-        onboardingView.setSnapshot(with: [
-            .list: [OnboardingFacilityView.Item.list(program)],
-                .button: [OnboardingFacilityView.Item.button]
-        ])
+//        onboardingView.setSnapshot(with: [
+//            .list: [OnboardingFacilityView.Item.list(program)],
+//                .button: [OnboardingFacilityView.Item.button]
+//        ])
     }
     
     override func bind(reactor: OnboardingReactor) {
