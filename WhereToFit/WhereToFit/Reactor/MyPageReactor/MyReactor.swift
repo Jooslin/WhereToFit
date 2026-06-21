b60b58bf2aeb9f856efe1a6cc3d9c65c58cee705
yeseul jang <yyssjj9999@gmail.com>
2026-06-21 20:14:42 +0900

    🌟feat: 마이 페이지 리액터 아이클라우드 활성화 상태 표시 추가

--- WhereToFit/WhereToFit/Reactor/MyPageReactor/MyReactor.swift
+++ WhereToFit/WhereToFit/Reactor/MyPageReactor/MyReactor.swift
@@ -5,10 +5,45 @@
 //  Created by Yeseul Jang on 6/2/26.
 //
 import ReactorKit
+import RxSwift
 
 final class MyReactor: BaseReactor {
     let initialState = State()
+    private let fetchICloudSyncStatusUseCase: FetchICloudSyncStatusUseCase
 
-    enum Action {}
-    struct State {}
+    init(fetchICloudSyncStatusUseCase: FetchICloudSyncStatusUseCase) {
+        self.fetchICloudSyncStatusUseCase = fetchICloudSyncStatusUseCase
+    }
+
+    enum Action {
+        case viewWillAppear
+    }
+
+    enum Mutation {
+        case setICloudSyncStatus(ICloudSyncStatus)
+    }
+
+    struct State {
+        var iCloudSyncStatus: ICloudSyncStatus = .needsAttention
+    }
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .viewWillAppear:
+            return fetchICloudSyncStatusUseCase.execute()
+                .asObservable()
+                .map(Mutation.setICloudSyncStatus)
+        }
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+
+        switch mutation {
+        case .setICloudSyncStatus(let status):
+            newState.iCloudSyncStatus = status
+        }
+
+        return newState
+    }
 }
