65781d57bc1375b8a8ca4704159e392eabc21e95
yeseul jang <yyssjj9999@gmail.com>
2026-06-21 20:13:29 +0900

    🌟feat: 플로우 변환시 레포지토리, 리액터 전달 추가

--- WhereToFit/WhereToFit/App/Flow/CalendarFlow.swift
+++ WhereToFit/WhereToFit/App/Flow/CalendarFlow.swift
@@ -11,7 +11,12 @@ import ReactorKit
 
 final class CalendarFlow: Flow {
     private let navigationController = UINavigationController()
-    private let calendarReactor = CalendarReactor()
+    private let calendarRecordRepository = CoreDataCalendarRecordRepository()
+    private lazy var calendarReactor = CalendarReactor(
+        saveWeightRecordUseCase: SaveWeightRecordUseCase(repository: calendarRecordRepository),
+        saveConditionRecordUseCase: SaveConditionRecordUseCase(repository: calendarRecordRepository),
+        fetchCalendarDayRecordsUseCase: FetchCalendarDayRecordsUseCase(repository: calendarRecordRepository)
+    )
     private lazy var calendarViewController = CalendarViewController(reactor: calendarReactor)
     var root: any RxFlow.Presentable { navigationController }
     
@@ -84,8 +89,14 @@ private extension CalendarFlow {
         else { return }
 
         let viewController = ExerciseRecordInputViewController(
-            reactor: ExerciseRecordInputReactor(selectedDate: calendarReactor.currentState.selectedDate)
+            reactor: ExerciseRecordInputReactor(
+                selectedDate: calendarReactor.currentState.selectedDate,
+                saveExerciseRecordUseCase: SaveExerciseRecordUseCase(repository: calendarRecordRepository)
+            )
         )
+        viewController.onSave = { [weak self] in
+            self?.calendarReactor.action.onNext(.refreshSelectedDate)
+        }
         viewController.modalPresentationStyle = .overFullScreen
         viewController.modalTransitionStyle = .crossDissolve
 
