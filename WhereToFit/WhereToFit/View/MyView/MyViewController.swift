28c24390a3874d6038dfc7b360b0f0495fb0632c
yeseul jang <yyssjj9999@gmail.com>
2026-06-22 13:09:46 +0900

    🌟feat: 마이페이지 고객지원 등 추가

--- WhereToFit/WhereToFit/View/MyView/MyViewController.swift
+++ WhereToFit/WhereToFit/View/MyView/MyViewController.swift
@@ -66,6 +66,41 @@ final class MyViewController: BaseViewController<MyReactor> {
             }
             .disposed(by: disposeBag)
 
+        myView.privacyPolicyAccessoryButton.rx.tap
+            .throttle(.milliseconds(500), latest: false, scheduler: MainScheduler.instance)
+            .bind(with: self) { owner, _ in
+                owner.openPrivacyPolicy()
+            }
+            .disposed(by: disposeBag)
+
+        myView.termsOfServiceAccessoryButton.rx.tap
+            .throttle(.milliseconds(500), latest: false, scheduler: MainScheduler.instance)
+            .bind(with: self) { owner, _ in
+                owner.openTermsOfService()
+            }
+            .disposed(by: disposeBag)
+
+        myView.locationTermsAccessoryButton.rx.tap
+            .throttle(.milliseconds(500), latest: false, scheduler: MainScheduler.instance)
+            .bind(with: self) { owner, _ in
+                owner.openLocationTerms()
+            }
+            .disposed(by: disposeBag)
+
+        myView.openSourceLicenseAccessoryButton.rx.tap
+            .throttle(.milliseconds(500), latest: false, scheduler: MainScheduler.instance)
+            .bind(with: self) { owner, _ in
+                owner.openOpenSourceLicense()
+            }
+            .disposed(by: disposeBag)
+
+        myView.inquiryAccessoryButton.rx.tap
+            .throttle(.milliseconds(500), latest: false, scheduler: MainScheduler.instance)
+            .bind(with: self) { owner, _ in
+                owner.openInquiryMail()
+            }
+            .disposed(by: disposeBag)
+
         reactor.state
             .map(\.iCloudSyncStatus)
             .distinctUntilChanged()
@@ -92,6 +127,50 @@ final class MyViewController: BaseViewController<MyReactor> {
 private extension MyViewController {
     func openAppSettings() {
         guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+
+        UIApplication.shared.open(url)
+    }
+
+    func openPrivacyPolicy() {
+        openURL("https://leaflog.notion.site/WhereToFit-3874589f9d0f808087f8d5c0d1343eb8?source=copy_link")
+    }
+
+    func openTermsOfService() {
+        openURL("https://leaflog.notion.site/WhereToFit-3874589f9d0f80e4b532fa1071613f15?source=copy_link")
+    }
+
+    func openLocationTerms() {
+        openURL("https://leaflog.notion.site/WhereToFit-3874589f9d0f80e4b532fa1071613f15?pvs=143")
+    }
+
+    func openOpenSourceLicense() {
+        openURL("https://leaflog.notion.site/WhereToFit-3874589f9d0f802ab105db88227ff13c?source=copy_link")
+    }
+
+    func openInquiryMail() {
+        var components = URLComponents()
+        components.scheme = "mailto"
+        components.path = "leaflogapp@gmail.com"
+        components.queryItems = [
+            URLQueryItem(name: "subject", value: "WhereToFit 문의")
+        ]
+
+        guard let url = components.url else { return }
+
+        UIApplication.shared.open(url, options: [:]) { [weak self] isOpened in
+            guard isOpened == false else { return }
+
+            self?.steps.accept(
+                AppStep.alert(
+                    title: "메일 앱을 열 수 없어요",
+                    message: "기본 메일 앱 설정을 확인하거나 leaflogapp@gmail.com으로 문의해주세요."
+                )
+            )
+        }
+    }
+
+    func openURL(_ urlString: String) {
+        guard let url = URL(string: urlString) else { return }
 
         UIApplication.shared.open(url)
     }
